a8ed0c81223f1337864848ba3d69740c8f3ad108
cloud <gunawan092w@gmail.com>
2025-04-13 23:36:16 +0700

    v2.1.0-pre
    Release Date: April 13, 2025
    
    [Changelogs]
    -> Bot now properly connects to VM
    -> Bot Token Authentications
    -> Origin header
    
    [Known Issues]
    -> Commands that requires special permissions are not working properly due to changes to some codes in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // cvmBotJS by Gunawan092w [https://github.com/gunawan092w/cvmbotJS]
 
-const WSClient = require('websocket').client; //dont stay logged in on hyperbeam, were just nice and didnt trash this
+const WSClient = require('websocket').client;
 const toml = require('toml');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -22,6 +22,8 @@ fs.readdirSync('./files/commands').forEach(file => {
 
 let readline;
 let permissions;
+let botrank;
+
 function bot() {
 	Client.removeAllListeners(); // Removes remaining listener
 	readline = require('readline').createInterface({input: process.stdin, output: process.stdout});
@@ -29,42 +31,37 @@ function bot() {
 	Client.on('connectFailed', reconnect);
 	Client.on('connect', client => {
 		client.on('close', reconnect);
-		
 		client.on('error',error => {console.log(`An error has accoured. {${error.toString()}}`)});
 
 		try {
 			function send(msg){client.sendUTF(msg)} // Added in v2.0.3
 
-			console.log(`Connected to VM!`); // Connection established
 			send(encode(['rename',config.bot.user])); // Bot sets Username
-
+			send(encode(['connect',config.bot.vmid]));
+			send(encode(['login',config.login.token]));
+			
 			function chat(message){send(encode(['chat',message]))}; // Chat
 			function xss(message){send(encode(["admin","21",message]))}; // XSS
 
 			function chatsession() {
 				readline.question('Chat: ',(msg)=>{
-					if (botrole==="mod") {
+					if (botrank==="mod") {
 						if (permissions.xss){xss(msg)} else(chat(msg));
-					} else if (botrole==="admin"){xss(msg)} else{chat(msg)};
+					} else if (botrank==="admin"){xss(msg)} else{chat(msg)};
 					chatsession(); // In loop.
 				})
 			}; chatsession();
-		
-			// Check if bot has been disallowed to login
-			let botrole;
-			if (config.settings.login==="false"){
-				console.log("Not logging in as mod / admin."); botrole="null"
-			} else{send(encode(['admin','2',config.bot.login]))} // If not, Bot proceeds to log in
 
 			// Startup Message. Enable/Disable in Config.
 			if (config.settings.startup==="true") {if(client.connected){chat(`${config.settings.startupmsg}`)}}; 
-
 			client.on('message', message =>{
 				const cmd = decode(message.utf8Data);
 				const action = cmd[0];
 				const prefix = config.bot.prefix;
 
-				if(action==="nop"){send(encode(['nop']))}; // Send Heartbeat
+				if(action==="nop"){send(encode(['nop']))}; // keepalive
+
+				if(action==="connect" && cmd[1]==='0'){console.log("Unable to connect, Please recheck / specify your VM-ID in config.toml!");process.exit(0)};
 
 				if (action==="adduser") { 
 					if (cmd[2]!==config.bot.user&&cmd[1]==="1") { // Ignore bot & Detect 1 user only. [v2.0.4]
@@ -75,27 +72,36 @@ function bot() {
 				};
 				if(action==="remuser"){console.log(`${cmd[2]} Left!`)}; // Logs user leaves.
 				if(action==="rename"&&cmd[1]==='1'){console.log(`${cmd[2]} Renamed to ${cmd[3]}`)}; // Detect if user renames
+
+				if (action==="login") {
+					if (cmd[1]==='0'){console.log(cmd[2])}
+						else {console.log(`Authenticated!`)}
+				};
 				
-				// Check if the bot has logged in as admin / mod
+				if(cmd[0]==='auth'){
+					console.log(`Authenticating...`);
+					send(encode(['login',config.login.token]));
+				}
+
 				if (action==="admin") {
-					if (cmd[2]==='0'){console.log("Incorrect login password!");botrole = "null"};
-					if (cmd[2]==='1'){console.log("Logged in as Administrator!");botrole = "admin"};
-					if (cmd[2]==='3'){console.log("Logged in as Moderator!"); botrole = "mod";
+					if (cmd[2]==='0'){console.log("Incorrect login password!");botrank = null};
+					if (cmd[2]==='1'){console.log("Logged in as Administrator!");botrank = "admin"};
+					if (cmd[2]==='3'){console.log("Logged in as Moderator!"); botrank = "mod";
 						permissions = new Permissions(cmd[3]); // Check Moderator Permissions
-					} module.exports = {botrole, permissions}; // export botrole, permissions
+					} module.exports = {botrank, permissions}; // export botrole, permissions
 				}
 
 				if(action==="chat"){
-					if(cmd[2] !=="") {
-						// https://github.com/computernewb/collabvm-1.2.ts/blob/master/cvmts/src/Utilities.ts Line 26
-						// Message Decoder to make it readable
-						const decodedmsg = cmd[2]
-						.replace(/&#x27;/g, "'")	.replace(/&quot;/g, '"')
-						.replace(/&#x2F;/g, "/")	.replace( /&lt;/g, "<" )
-						.replace( /&gt;/g, ">" )	.replace( /&amp;/g, "&");
-						console.log(`\n${cmd[1]} says: ${decodedmsg}`); // Output messages to console
-					} else {}; // Ignores if the message is empty (Happens if user send empty message through XSS)
-				
+					// https://github.com/computernewb/collabvm-1.2.ts/blob/master/cvmts/src/Utilities.ts Line 26
+					// Decode message to human-readable
+					const decodedmsg = cmd[2]
+					.replace(/&#x27;/g, "'")	.replace(/&quot;/g, '"')
+					.replace(/&#x2F;/g, "/")	.replace( /&lt;/g, "<" )
+					.replace( /&gt;/g, ">" )	.replace( /&amp;/g, "&");
+					
+					if(cmd[2]!==""&&cmd[1]!=="") {console.log(`\n${cmd[1]} says: ${decodedmsg}`)} else{};
+					if(cmd[1]===""){console.log(`\n${decodedmsg}`)} else{};
+					
 					const cmdName = cmd[2].slice(prefix.length).trim().split(' ')[0];
                 	if(cmd[1]!==config.bot.user){ // Ignore bot messages
 						if (cmd[2].startsWith(prefix) && command[cmdName]){
@@ -107,13 +113,13 @@ function bot() {
 				};
 
 				// Debugger
-				if (config.settings.debug === 'true' && cmd[0] !== 'nop') { // Check if debug is true in config & ignore 'nop'
-					console.log(cmd); //Outputs WebSocket Messages through console.
-				};
+				if (config.settings.debug === 'true' && !['nop', 'png', 'async', 'sync', 'turn', 'size'].includes(cmd[0])) {console.log(cmd)};
 				
 			});
-		} catch (error){chat (`Something went wrong, check console for more details.`); console.log(error)}
-	});  Client.connect(config.bot.ip, 'guacamole'); // Bot connects to VM
+		} catch (error){console.log(error)}
+	});  Client.connect(config.bot.ip, 'guacamole', null, {
+		origin: config.bot.origin
+	}); // Bot connects to VM
 }
 
 console.log('Connecting to VM...');
